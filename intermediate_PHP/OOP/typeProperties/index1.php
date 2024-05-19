13dddc007219fb641fdc6b7292a5efd7dd363469
DaryllCulas <culasdaryll99@gmail.com>
2024-05-20 03:39:49 +0800

    Add chaining method

--- intermediate_PHP/OOP/typeProperties/index1.php
+++ intermediate_PHP/OOP/typeProperties/index1.php
@@ -3,12 +3,23 @@
 require_once('../typeProperties/transactions.php');
 
 
-$objTransaction1 = new Transaction(100, '2020-01-01');
+$objTransaction1 = (new Transaction(100, '2020-01-01'))
+  ->addTax(7)
+  ->applyDiscount(10)
+  ->getAmount();
 
 
-$objTransaction1->addTax(8);
-$objTransaction1->applyDiscount(10);
+
+print("$$objTransaction1");
+
+
+
+
+
+// $objTransaction1->addTax(8);
+// $objTransaction1->applyDiscount(10);
+
 
 // print("$$objTransaction1->amount = Date: $objTransaction1->description\n");
 
-var_dump($objTransaction1->getAmount());
+// var_dump($objTransaction1->getAmount());
