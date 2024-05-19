5150ba664aaaf51feb0445d4619d2ca8357e87d8
DaryllCulas <culasdaryll99@gmail.com>
2024-05-20 06:31:47 +0800

    Add obj for null safe operator

--- intermediate_PHP/OOP/constructorPropertyPromotion/index2.php
+++ intermediate_PHP/OOP/constructorPropertyPromotion/index2.php
@@ -1,6 +1,16 @@
 <?php
 
 require_once('./constructorPropertyPromotion.php');
+require_once('./nullSafeOperator.php');
+
+
+
 $objConstructorHere = new Point();
 
 print_r($objConstructorHere);
+
+
+// This is for null safe operations
+$empObj = new Emp();
+$dept = $empObj?->getAddress()?->dept?->iso_code;
+print_r($dept);
