d35df73e897e485170911cfe31aa01eb83d38b85
DaryllCulas <culasdaryll99@gmail.com>
2024-05-21 03:31:42 +0800

    Modified for Email

--- intermediate_PHP/OOP/nameSpace/paddle/transaction.php
+++ intermediate_PHP/OOP/nameSpace/paddle/transaction.php
@@ -5,8 +5,20 @@ declare(strict_types=1);
 
 namespace PaymentGateway\Paddle;
 
+use Paddle\CustomerProfile;
+use DateTime;
+use Email;
+
 class Transaction
 {
+  public function __construct()
+  {
+    // var_dump(new CustomerProfile);
+    // print_r(new \DateTime('now'));
+    // print_r(new DateTime);
+    print_r(new Email);
+  }
+
   public function transaction1()
   {
     echo "This is transaction 1";
