69c84aaa6bf2d7f04d342f788e60c1d395bd1843
DaryllCulas <culasdaryll99@gmail.com>
2024-05-21 03:53:25 +0800

    Modified

--- intermediate_PHP/OOP/nameSpace/index3.php
+++ intermediate_PHP/OOP/nameSpace/index3.php
@@ -9,7 +9,21 @@ require_once('../nameSpace/notification/email.php');
 // print_r(new \PaymentGateway\Stripe\Transaction());
 
 
-use \PaymentGateway\Paddle\Transaction;
+// use \PaymentGateway\Paddle\Transaction;
+use \PaymentGateway\Paddle;
+use \PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+// print_r(new Transaction());
+
+$paddleTransaction = new Paddle\Transaction();
+$stripeTransaction = new StripeTransaction();
+$paddleCustomerProfile = new Paddle\CustomerProfile();
+
+print_r($paddleTransaction);
+print_r($stripeTransaction);
+print_r($paddleCustomerProfile);
 
 
-print_r(new Transaction());
+// you can var_dump with multiple objects (classes)
+
+// var_dump($paddleTransaction, $stripeTransaction, $paddleCustomerProfile);
