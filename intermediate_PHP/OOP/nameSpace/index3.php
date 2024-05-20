17978e3801530bf8c90cb9772bd36dd31c8c51a4
DaryllCulas <culasdaryll99@gmail.com>
2024-05-21 03:31:03 +0800

    Modified

--- intermediate_PHP/OOP/nameSpace/index3.php
+++ intermediate_PHP/OOP/nameSpace/index3.php
@@ -2,7 +2,14 @@
 
 require_once('../nameSpace/stripe/transaction.php');
 require_once('../nameSpace/paddle/transaction.php');
+require_once('../nameSpace/paddle/customerProfile.php');
+require_once('../nameSpace/notification/email.php');
+
+// print_r(new \PaymentGateway\Paddle\Transaction());
+// print_r(new \PaymentGateway\Stripe\Transaction());
 
 
-print_r(new \PaymentGateway\Paddle\Transaction());
-print_r(new \PaymentGateway\Stripe\Transaction());
+use \PaymentGateway\Paddle\Transaction;
+
+
+print_r(new Transaction());
