b5c9fdab5cb65fa1a48a1e7bfd53ef42fd87c878
DaryllCulas <culasdaryll99@gmail.com>
2024-05-22 01:37:14 +0800

    Create transaction for Stripe directory && Put namespace as \PaymentGatewayV2\Stripe\TransactionV2

---
+++ intermediate_PHP/OOP/codingStandards_autoloadPSR_composer/stripe2/transaction.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PaymentGatewayV2\Stripe;
+
+use Notification2\Email\EmailV2;
+
+class TransactionV2
+{
+
+  public function __constructor()
+  {
+    var_dump(new EmailV2());
+  }
+}
