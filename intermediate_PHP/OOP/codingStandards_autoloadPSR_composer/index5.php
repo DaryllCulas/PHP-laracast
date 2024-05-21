e34584115a3da239f1cfbb522d169bb9a2811628
DaryllCulas <culasdaryll99@gmail.com>
2024-05-22 00:40:50 +0800

    Using composer classes in indexfile

--- intermediate_PHP/OOP/codingStandards_autoloadPSR_composer/index5.php
+++ intermediate_PHP/OOP/codingStandards_autoloadPSR_composer/index5.php
@@ -1,1 +1,20 @@
 <?php
+
+
+
+
+
+
+
+
+
+// Using composer packages
+use Ramsey\Uuid\UuidFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// $id = new \Ramsey\Uuid\UuidFactory(); // Prepended
+$id = new UuidFactory();
+
+
+echo $id->uuid4();
