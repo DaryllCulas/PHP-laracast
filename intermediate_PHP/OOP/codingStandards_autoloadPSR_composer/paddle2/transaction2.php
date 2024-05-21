df26fc2444622b4d6c57a66896910243ef27c630
DaryllCulas <culasdaryll99@gmail.com>
2024-05-22 01:34:59 +0800

    Create transaction class && put namespace as PaymentGateway\Paddle\Transaction2

---
+++ intermediate_PHP/OOP/codingStandards_autoloadPSR_composer/paddle2/transaction2.php
@@ -0,0 +1,16 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace PaymentGatewayV2\Paddle;
+
+class Transaction2
+{
+
+
+  public function __construct()
+  {
+    var_dump(new CustomerProfileV2());
+  }
+}
