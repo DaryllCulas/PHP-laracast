0e634b8bff97c22a5c0e06b31a9375f804b29942
DaryllCulas <culasdaryll99@gmail.com>
2024-05-24 03:17:45 +0800

    encapsulation added using private access modifiers and reflection api

---
+++ intermediate_PHP/OOP/encapsulation_and_abstraction/PaymentGateway8/transaction.php
@@ -0,0 +1,45 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace PaymentGateway8;
+
+use DateTime;
+
+class Transaction8
+{
+
+  private float $amount;
+  private bool $isEmailSent;
+  private string $isEmailSentToString;
+
+  public function __construct(float $amount, string $isEmailSentToString)
+  {
+    $this->amount = $amount;
+    $this->isEmailSent = true;
+    $this->isEmailSentToString = $isEmailSentToString;
+  }
+
+  public function process()
+  {
+    echo "Processing $$this->amount: Transaction completed";
+    $this->generateReceipt();
+    echo PHP_EOL;
+    $this->sendEmail();
+  }
+
+  private function generateReceipt()
+  {
+    $datetime = new DateTime("2023-01-01 11:00:00", new \DateTimeZone("UTC"));
+
+    return "\nDate:" . $datetime->format("Y-m-d H:i:s") . "\nAmount:" . $this->amount . "\n";
+  }
+  private function sendEmail()
+  {
+    if ($this->isEmailSent === 1) {
+      $this->isEmailSentToString = "True";
+    }
+    return "is email sent? =" . $this->isEmailSentToString;
+  }
+}
