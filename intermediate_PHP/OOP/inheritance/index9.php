01bbb211defa0d25bc420dcc4133ba3e7a9856ed
DaryllCulas <culasdaryll99@gmail.com>
2024-05-25 03:09:25 +0800

    Modified

--- intermediate_PHP/OOP/inheritance/index9.php
+++ intermediate_PHP/OOP/inheritance/index9.php
@@ -1,3 +1,19 @@
 <?php
 
+
 require_once(__DIR__ . '/Toaster.php');
+require_once(__DIR__ . '/ToasterPro.php');
+
+use App\Inheritance\Toaster;
+use App\Inheritance\ToasterPro;
+
+$ToasterObj = new ToasterPro(); // Instantiated object of child class ToasterPro
+
+$ToasterObj->addSlice('bread');
+$ToasterObj->addSlice('flour');
+$ToasterObj->addSlice('eggs');
+$ToasterObj->addSlice('milk');
+$ToasterObj->addSlice('sugar');
+$ToasterObj->addSlice('butter');
+$ToasterObj->addSlice('cocoa');
+$ToasterObj->toastBagel();
