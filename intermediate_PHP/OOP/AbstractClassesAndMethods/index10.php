82d33a725c2aa8788789a3c5b9e46abd6a4a12f0
DaryllCulas <culasdaryll99@gmail.com>
2024-05-25 18:05:54 +0800

    Modified and removed some instatiated obj

--- intermediate_PHP/OOP/AbstractClassesAndMethods/index10.php
+++ intermediate_PHP/OOP/AbstractClassesAndMethods/index10.php
@@ -1,1 +1,23 @@
 <?php
+
+
+require_once(__DIR__ . '/field.php');
+require_once(__DIR__ . '/text.php');
+require_once(__DIR__ . '/boolean.php');
+require_once(__DIR__ . '/radio.php');
+require_once(__DIR__ . '/checkbox.php');
+
+
+
+$fields = [
+
+  new \App\AbstractClassesAndMethods\Text('textField'),
+  new \App\AbstractClassesAndMethods\CheckBox('checkBoxField'),
+  new \App\AbstractClassesAndMethods\Radio('radioField'),
+];
+
+
+
+foreach ($fields as $field) {
+  echo $field->render() . '<br/>';
+}
