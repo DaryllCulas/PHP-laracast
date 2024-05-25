3a8bf2596e1efe433b861ea672a2981e5390c742
DaryllCulas <culasdaryll99@gmail.com>
2024-05-25 18:04:59 +0800

    Modified class into abstract class and method

--- intermediate_PHP/OOP/AbstractClassesAndMethods/field.php
+++ intermediate_PHP/OOP/AbstractClassesAndMethods/field.php
@@ -1,1 +1,16 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\AbstractClassesAndMethods;
+
+
+abstract class Field
+{
+
+  public function __construct(protected string $name)
+  {
+  }
+
+  abstract public function render(): string;
+}
