8e2c3049fcaed9a82545e6cf7425c8b1d149a713
DaryllCulas <culasdaryll99@gmail.com>
2024-05-25 18:06:16 +0800

    Modified

--- intermediate_PHP/OOP/AbstractClassesAndMethods/text.php
+++ intermediate_PHP/OOP/AbstractClassesAndMethods/text.php
@@ -1,1 +1,17 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\AbstractClassesAndMethods;
+
+
+class Text extends Field
+{
+
+  public function render(): string
+  {
+    return <<<HTML
+    <input type="text" name="{$this->name}" value="{$this->name}"/>
+    HTML;
+  }
+}
