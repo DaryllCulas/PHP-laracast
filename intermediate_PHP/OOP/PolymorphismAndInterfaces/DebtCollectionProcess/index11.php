8c652342659abc35cdd80aaa11f7c3db97ad3cf1
DaryllCulas <culasdaryll99@gmail.com>
2024-05-26 06:08:49 +0800

    Modified

--- intermediate_PHP/OOP/PolymorphismAndInterfaces/DebtCollectionProcess/index11.php
+++ intermediate_PHP/OOP/PolymorphismAndInterfaces/DebtCollectionProcess/index11.php
@@ -1,1 +1,18 @@
 <?php
+
+use App\PolymorphismInterfaces\CollectionAgency;
+use App\PolymorphismInterfaces\DebtCollectionService;
+use App\PolymorphismInterfaces\RockyTheDebtCollector;
+
+require_once(__DIR__ . '/DebtCollector.php');
+require_once(__DIR__ . '/CollectionAgency.php');
+require_once(__DIR__ . '/DebtCollectionService.php');
+require_once(__DIR__ . '/RockyTheDebtCollector.php');
+
+
+// $collector = new CollectionAgency();
+// echo "Amount collected: {$collector->collect(100)}" . PHP_EOL;
+
+$service = new DebtCollectionService();
+// echo $service->collectDebt(new CollectionAgency()) . PHP_EOL;
+echo $service->collectDebt(new RockyTheDebtCollector()) . PHP_EOL;
