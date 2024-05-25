435510ede112e1cc4e31e7bd8dbf960aed7a4c22
DaryllCulas <culasdaryll99@gmail.com>
2024-05-25 18:05:29 +0800

    Modified

--- intermediate_PHP/OOP/AbstractClassesAndMethods/radio.php
+++ intermediate_PHP/OOP/AbstractClassesAndMethods/radio.php
@@ -1,1 +1,17 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\AbstractClassesAndMethods;
+
+
+class Radio extends Boolean
+{
+
+  public function render(): string
+  {
+    return <<<HTML
+    <input type="radio" name="{$this->name}" value="{$this->name}"/>
+    HTML;
+  }
+}
