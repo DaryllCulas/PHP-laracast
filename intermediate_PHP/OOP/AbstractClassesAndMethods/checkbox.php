70a7230f74beb80af380cc791aabe4affbe6bbda
DaryllCulas <culasdaryll99@gmail.com>
2024-05-25 18:04:28 +0800

    Modified

--- intermediate_PHP/OOP/AbstractClassesAndMethods/checkbox.php
+++ intermediate_PHP/OOP/AbstractClassesAndMethods/checkbox.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace App\AbstractClassesAndMethods;
 
 
-class CheckBox
+class CheckBox extends Boolean
 {
+  public function render(): string
+  {
+    return <<<HTML
+    <input type="checkbox" name="{$this->name}" value="{$this->name}"/>
+    HTML;
+  }
 }
