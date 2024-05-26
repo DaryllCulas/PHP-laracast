7e532e9c9fe194e8e52194b60b277ef917d2e7e4
DaryllCulas <culasdaryll99@gmail.com>
2024-05-26 18:25:04 +0800

    Added and modified

---
+++ intermediate_PHP/OOP/MagicMethods/index12.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\MagicMethods\Invoice;
+use App\MagicMethods\Invoice2;
+
+require_once(__DIR__ . '/magicMethods.php');
+
+
+// $invoiceObj = new Invoice();
+// $invoiceObj->amount = 150;
+
+// var_dump(isset($invoiceObj->amount));
+// var_dump($invoiceObj);
+
+// unset($invoice->amount);
+// var_dump($invoiceObj);
+
+
+
+$invoiceObj2 = new Invoice2();
+$invoiceObj2->process(1, 2, 3, 5);
+$invoiceObj2::process(1, 2, 3, 5);
+$invoiceObj2->process2(45, 'This is description');
+echo PHP_EOL;
+echo $invoiceObj2;
+echo PHP_EOL;
+$invoiceObj2();
+var_dump(is_callable($invoiceObj2));
+echo PHP_EOL;
+print_r($invoiceObj2);
---
+++ intermediate_PHP/OOP/MagicMethods/magicMethods.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\MagicMethods;
+
+class Invoice
+{
+  protected array $data = [];
+
+  public function __get(string $name)
+  {
+    if (array_key_exists($name, $this->data)) {
+      return $this->data[$name];
+    }
+    return null;
+  }
+
+
+  public function __set(string $name, $value): void
+  {
+    var_dump('isset');
+    $this->data[$name] = $value;
+  }
+
+
+
+  public function __isset(string $name): bool
+  {
+    var_dump('unset');
+    return array_key_exists($name, $this->data);
+  }
+  public function __unset(string $name): void
+  {
+    var_dump('unset');
+    unset($this->data[$name]);
+  }
+}
+
+
+class Invoice2
+{
+  private float $amountTobePaid = 343.34;
+  private int $id = 23;
+  private string $accountNumber = 'ff23343c3';
+
+  protected function process2(float $amount, $description)
+  {
+    var_dump($amount, $description);
+  }
+  public function __call(string $name, array $arguments)
+  {
+    if (method_exists($this, $name)) {
+      call_user_func_array([$this, $name], $arguments);
+      $this->name($arguments);
+    }
+  }
+  public static function __callStatic(string $name, array $arguments)
+  {
+    var_dump('static', $name, $arguments);
+  }
+
+  public function __toString(): string
+  {
+    // return 1;
+    return 'Hello world';
+  }
+
+  public function __invoke()
+  {
+    var_dump('Invoked');
+  }
+
+  public function __debugInfo(): ?array
+  {
+    return [
+      'id' => $this->id,
+      'amount_to_be_paid' => $this->amountTobePaid,
+      'account_number' => '****' . substr($this->accountNumber, -4),
+    ];
+  }
+}
