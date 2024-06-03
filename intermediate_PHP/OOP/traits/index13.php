b6007fe6652d7eaf31818f0fe431a93b4ede2380
DaryllCulas <culasdaryll99@gmail.com>
2024-06-04 04:17:23 +0800

    modified -> alternate precedence

--- intermediate_PHP/OOP/traits/index13.php
+++ intermediate_PHP/OOP/traits/index13.php
@@ -1,7 +1,11 @@
 <?php
+
 require_once(__DIR__ . '/PrecedenceOrder/sayHello.php');
 require_once(__DIR__ . '/PrecedenceOrder/sayWorld.php');
 require_once(__DIR__ . '/PrecedenceOrder/myHelloWorld.php');
+require_once(__DIR__ . '/AlternatePrecedenceOrder/traitHelloWorld.php');
+require_once(__DIR__ . '/AlternatePrecedenceOrder/TheWorldIsNotEnough.php');
+
 
 
 
@@ -11,3 +15,7 @@ use PrecedenceOrder\Base\MyHelloWorld;
 
 $callHelloWorld = new MyHelloWorld();
 $callHelloWorld->sayHello();
+
+echo PHP_EOL;
+$altPrecedenceWorld = new TheWorldIsNotEnough();
+$altPrecedenceWorld->sayHello2();
