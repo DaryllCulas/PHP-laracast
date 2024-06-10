8514a20b0a498d22c2dc5815cbd354eafb3aca39
DaryllCulas <culasdaryll99@gmail.com>
2024-06-10 10:23:55 +0800

    Modified

--- intermediate_PHP/OOP/traits/index13.php
+++ intermediate_PHP/OOP/traits/index13.php
@@ -5,16 +5,29 @@ require_once(__DIR__ . '/PrecedenceOrder/sayWorld.php');
 require_once(__DIR__ . '/PrecedenceOrder/myHelloWorld.php');
 require_once(__DIR__ . '/AlternatePrecedenceOrder/traitHelloWorld.php');
 require_once(__DIR__ . '/AlternatePrecedenceOrder/TheWorldIsNotEnough.php');
+require_once(__DIR__ . '/MultipleTraitsUsage/traitHello.php');
+require_once(__DIR__ . '/MultipleTraitsUsage/traitWorld.php');
+require_once(__DIR__ . '/MultipleTraitsUsage/MyHelloWorld.php');
 
 
 
 
+// Precedence Order
 use PrecedenceOrder\Base\MyHelloWorld;
-
 
 $callHelloWorld = new MyHelloWorld();
 $callHelloWorld->sayHello();
 
 echo PHP_EOL;
+
+// Alternate Precedence Order
 $altPrecedenceWorld = new TheWorldIsNotEnough();
 $altPrecedenceWorld->sayHello2();
+
+echo PHP_EOL;
+
+// Multiple traits usage
+$combinedHW = new MyHelloWorldCombined();
+$combinedHW->sayHello();
+$combinedHW->sayWorld();
+$combinedHW->sayExclamationMark();
