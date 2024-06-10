3eaae7d02a9d8379bcfbe410b36e06f5319b36ee
DaryllCulas <culasdaryll99@gmail.com>
2024-06-11 02:08:25 +0800

    Modified -> static functions

--- intermediate_PHP/OOP/traits/CoffeeShop/LatteTrait.php
+++ intermediate_PHP/OOP/traits/CoffeeShop/LatteTrait.php
@@ -4,8 +4,20 @@ namespace App;
 
 trait LatteTrait
 {
+  private string $milkType = 'whole milk';
   public function makeLatte()
   {
-    echo static::class . ' is making latte ' . PHP_EOL;
+    echo static::class . ' is making latte with ' . $this->milkType . PHP_EOL;
+  }
+
+  public static function mixLatte()
+  {
+    echo 'Mixing latte by ' . static::class;
+  }
+
+  public function setMilkType(string $milkType): static
+  {
+    $this->milkType = $milkType;
+    return $this;
   }
 }
