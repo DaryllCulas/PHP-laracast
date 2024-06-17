2acca26e45cd2cc7dda11c1b340335c5c4450739
DaryllCulas <culasdaryll99@gmail.com>
2024-06-18 01:49:05 +0800

    Modified => setting up a upload() for file uploading functionality

--- intermediate_PHP/OOP/PHP_File_uploads/BasicRoutings/Home.php
+++ intermediate_PHP/OOP/PHP_File_uploads/BasicRoutings/Home.php
@@ -20,7 +20,7 @@ class Home
     );
 
     $_SESSION['count'] = ($_SESSION['count'] ?? 0) + 1;
-    return '<form>
+    return '<form action="/upload" method="post" enctype="multipart/form-data">
       <input type="file" name="receipt"/>
       <button type="submit">Upload</button>
     </form>';
@@ -28,5 +28,17 @@ class Home
 
   public function upload()
   {
+    echo '<pre>';
+    var_dump($_FILES);
+
+    echo '</pre>';
+
+
+    $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
+    move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
+
+    echo '<pre>';
+    var_dump(pathinfo($filePath));
+    echo '</pre>';
   }
 }
