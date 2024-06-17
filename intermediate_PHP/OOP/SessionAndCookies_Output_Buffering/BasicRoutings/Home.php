b0034ba65ef479cddb99cf736469b7f676057835
DaryllCulas <culasdaryll99@gmail.com>
2024-06-18 01:16:53 +0800

    Set cookie's values

--- intermediate_PHP/OOP/SessionAndCookies_Output_Buffering/BasicRoutings/Home.php
+++ intermediate_PHP/OOP/SessionAndCookies_Output_Buffering/BasicRoutings/Home.php
@@ -9,6 +9,16 @@ class Home
 {
   public function index(): string
   {
+    setcookie(
+      'userName',
+      'Makarov',
+      time() + 10,
+      '/',
+      '',
+      false,
+      false
+    );
+
     $_SESSION['count'] = ($_SESSION['count'] ?? 0) + 1;
     return 'Home';
   }
