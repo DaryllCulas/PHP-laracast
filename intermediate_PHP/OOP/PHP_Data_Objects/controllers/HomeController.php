170662eefe2a85562a4aba0c66fbf40a020beaa5
DaryllCulas <culasdaryll99@gmail.com>
2024-06-21 03:58:54 +0800

    apply emulate prepares

--- intermediate_PHP/OOP/PHP_Data_Objects/controllers/HomeController.php
+++ intermediate_PHP/OOP/PHP_Data_Objects/controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController
 
     try {
       var_dump($_ENV['DB_HOST']);
-      $db = new PDO('mysql:host=localhost:3307;dbname=TestDB', 'root', '', [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+      $db = new PDO('mysql:host=localhost:3307;dbname=TestDB', 'root', '', [PDO::ATTR_EMULATE_PREPARES => false]);
 
       // SQL INJECTION if you want type this: http://localhost:3304/?userEmail=niceone@gmail.com%22+OR+1=1+--+
 
@@ -27,20 +27,21 @@ class HomeController
 
 
 
-      $userEmail = 'Gerardway2@gmail.com';
-      $full_name = 'Gerard way';
+      $userEmail = 'hard away4@gmail.com';
+      $full_name = 'hard away III';
       $is_active = 1;
       $created_at = date('Y-m-d H:i:s', strtotime('07/11/2021 12:00PM'));
 
 
-      $query = 'INSERT INTO users(userEmail, full_name, is_active, created_at, updated_at) VALUES (:userEmail, :full_name, :is_active, :created_at, :created_at)';
+      $query = 'INSERT INTO users(userEmail, full_name, is_active, created_at, updated_at) VALUES (:userEmail, :full_name, :is_active, :created_at1, :created_at2)';
 
       $stmt = $db->prepare($query);
 
       $stmt->bindValue('userEmail', $userEmail);
       $stmt->bindValue('full_name', $full_name);
       $stmt->bindValue('is_active', $is_active, PDO::PARAM_BOOL);
-      $stmt->bindValue('created_at', $created_at);
+      $stmt->bindValue('created_at1', $created_at);
+      $stmt->bindValue('created_at2', $created_at);
 
 
 
