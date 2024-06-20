18c2da132707f61fa66d1df72fc202d7a7eed5b5
DaryllCulas <culasdaryll99@gmail.com>
2024-06-21 07:54:13 +0800

    Refactored

--- intermediate_PHP/OOP/PDO_Models_and_Refactoring/public/index.php
+++ intermediate_PHP/OOP/PDO_Models_and_Refactoring/public/index.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use App\App;
+use App\Config;
 use App\Exceptions\RouteNotFoundException;
 use App\Router;
 
@@ -10,6 +12,9 @@ require_once(__DIR__ . '/../BasicRoutings/Exceptions/RouteException.php');
 require_once(__DIR__ . '/../controllers/HomeController.php');
 require_once(__DIR__ . '/../controllers/InvoiceController.php');
 require_once(__DIR__ . '/../views/View.php');
+require_once(__DIR__ . '/../App.php');
+require_once(__DIR__ . '/../DB.php');
+require_once(__DIR__ . '/../Config.php');
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 session_start();
@@ -21,24 +26,18 @@ define('STORAGE_PATH', __DIR__ . '/../storage');
 define('VIEW_PATH', __DIR__ . '/../views');
 
 
-try {
+$routerObj = new Router();
 
-  $routerObj = new Router();
+$routerObj->get('/', [\App\Controllers\HomeController::class, 'index'])
+  ->post('/upload', [\App\Controllers\HomeController::class, 'upload'])
+  ->get('/invoice', [\App\Controllers\InvoiceController::class, 'index'])
+  ->get('/invoice/create', [\App\Controllers\InvoiceController::class, 'create'])
+  ->post('/invoice/create', [\App\Controllers\InvoiceController::class, 'store']);
 
-  $routerObj->get('/', [\App\Controllers\HomeController::class, 'index'])
-    ->post('/upload', [\App\Controllers\HomeController::class, 'upload'])
-    ->get('/invoice', [\App\Controllers\InvoiceController::class, 'index'])
-    ->get('/invoice/create', [\App\Controllers\InvoiceController::class, 'create'])
-    ->post('/invoice/create', [\App\Controllers\InvoiceController::class, 'store']);
 
-  $requestUri = $_SERVER['REQUEST_URI'] ?? '/';
 
-  echo $routerObj->resolve($requestUri, strtolower($_SERVER['REQUEST_METHOD']));
-
-  var_dump($_SESSION);
-} catch (RouteNotFoundException $e) {
-
-  http_response_code(404);
-
-  echo \App\View::make('error/404');
-}
+(new App(
+  $routerObj,
+  ['uri' => $_SERVER['REQUEST_URI'], 'method' => $_SERVER['REQUEST_METHOD']],
+  new Config($_ENV)
+))->run();
