4da6a036819a0d2e73ffdb0e775a9ad039c7798f
DaryllCulas <culasdaryll99@gmail.com>
2024-06-21 03:35:32 +0800

    Modified => named parameters on prepare statements

--- intermediate_PHP/OOP/PHP_Data_Objects/controllers/HomeController.php
+++ intermediate_PHP/OOP/PHP_Data_Objects/controllers/HomeController.php
@@ -19,16 +19,36 @@ class HomeController
 
       // SQL INJECTION if you want type this: http://localhost:3304/?userEmail=niceone@gmail.com%22+OR+1=1+--+
 
-      $email = $_GET['userEmail'];
-      $query = 'SELECT * FROM users WHERE userEmail = "' . $email . '"';
-      echo $query . '<br/>';
+      // $email = $_GET['userEmail'];
 
-      foreach ($db->query($query) as $users) {
-        echo '<pre>';
-        var_dump($users);
-        echo '</pre>';
-      }
+      // $query = 'SELECT * FROM users WHERE userEmail = "' . $email . '"';
+
+      // echo $query . '<br/>';
+
+
+
+      $userEmail = 'johnnysins233@gmail.com';
+      $full_name = 'Johnny sin';
+      $isActive = 1;
+      $createdAt = date('Y-m-d H:i:s', strtotime('07/11/2021 12:00PM'));
+      $query = 'INSERT INTO users(userEmail, full_name, is_active, created_at) VALUES (:userEmail, :full_name, :isActive, :createdAt)';
+
+      $stmt = $db->prepare($query);
+      $stmt->execute([
+        'userEmail' => $userEmail,
+        'full_name' => $full_name,
+        'isActive' => $isActive,
+        'createdAt' => $createdAt
+      ]);
+
+      $id = (int) $db->lastInsertId();
+      $users = $db->query('SELECT * FROM users WHERE id = ' . $id)->fetch();
+
+      echo '<pre>';
+      var_dump($users);
+      echo '</pre>';
     } catch (PDOException $e) {
+
       throw new PDOException($e->getMessage(), (int)$e->getCode());
     }
 
