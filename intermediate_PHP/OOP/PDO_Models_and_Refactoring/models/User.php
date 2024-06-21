0f71d7fe6395d76d8ebc05b65893ac5630efcbad
DaryllCulas <culasdaryll99@gmail.com>
2024-06-21 09:53:56 +0800

    Placeholders fixed

--- intermediate_PHP/OOP/PDO_Models_and_Refactoring/models/User.php
+++ intermediate_PHP/OOP/PDO_Models_and_Refactoring/models/User.php
@@ -13,10 +13,9 @@ class User extends Model
 
   public function create(string $userEmail, string $full_name, $isActive = true): int
   {
-    $stmt = $this->db->prepare('INSERT INTO users (userEmail, full_name, is_active, created_at) VALUES (?, ?, 1, NOW())');
+    $stmt = $this->db->prepare('INSERT INTO users (userEmail, full_name, is_active, created_at) VALUES (?, ?, ?, NOW())');
 
     $stmt->execute([$userEmail, $full_name, $isActive]);
-
 
     return (int) $this->db->lastInsertId();
   }
