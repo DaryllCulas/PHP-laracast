ba7ede216ab662463972cd78b3d4b3013d47e576
DaryllCulas <culasdaryll99@gmail.com>
2024-07-02 06:03:42 +0800

    Mocking => Successful (2 tests, 3 assertions)

--- advance_PHP/PHP_Mocking/tests/Unit/Services/InvoiceServiceTest.php
+++ advance_PHP/PHP_Mocking/tests/Unit/Services/InvoiceServiceTest.php
@@ -35,10 +35,43 @@ class InvoiceServiceTest extends TestCase
       $emailServiceMock
     );
 
-    $customer = ['name' => 'Gio'];
+    $customer = ['name' => 'Mangkanor'];
     $amount = 150;
 
     // when process is called
+    $result = $invoiceService->process($customer, $amount);
+
+
+    // then assert invoice is processed properly
+    $this->assertTrue($result);
+  }
+
+  #[Test]
+  public function test_if_it_sends_receipt_email_when_invoice_is_processed(): void
+  {
+
+    $customer = ['name' => 'Mangkanor'];
+    $amount = 150;
+    $salesTaxServiceMock = $this->createMock(SalesTaxService::class);
+
+    $paymentGatewayServiceMock = $this->createMock(PaymentGatewayService::class);
+
+    $emailServiceMock = $this->createMock(EmailService::class);
+
+    // stubs
+    $paymentGatewayServiceMock->method('charge')->willReturn(true);
+
+    $emailServiceMock->expects($this->once())
+      ->method('send')
+      ->with(['name' => 'Mangkanor'], 'receipt');
+
+    // given invoice service
+    $invoiceService = new InvoiceService(
+      $salesTaxServiceMock,
+      $paymentGatewayServiceMock,
+      $emailServiceMock
+    );
+
     $result = $invoiceService->process($customer, $amount);
 
 
