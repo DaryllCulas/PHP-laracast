9b994dda8ba2d4e9de5eed533fff3bb0db40d235
DaryllCulas <culasdaryll99@gmail.com>
2024-07-04 05:24:26 +0800

    it sets the PaymentGatewayService Interface class in the container to be an instance of the PaddlePayment class

--- advance_PHP/DI_container_with_interface_support/app/App.php
+++ advance_PHP/DI_container_with_interface_support/app/App.php
@@ -9,10 +9,9 @@ use App\Controllers\Router;
 use App\DB;
 use App\Exceptions\RouteNotFoundException;
 use App\Services\Container;
-use App\Services\EmailService;
-use App\Services\InvoiceService;
+use App\Services\PaddlePayment;
 use App\Services\PaymentGatewayService;
-use App\Services\SalesTaxService;
+use App\Services\PaymentGatewayServiceInterface;
 use App\Views\View;
 
 class App
@@ -20,24 +19,14 @@ class App
   private static DB $db;
   // public static Container $container;
 
-  public function __construct(protected Router $routerObj, protected array $request, protected Config $config)
+  public function __construct(protected Container $container, protected Router $routerObj, protected array $request, protected Config $config)
   {
     static::$db = new DB($config->db ?? []);
-    // static::$container = new Container();
 
-    // static::$container->set(
-    //   InvoiceService::class,
-    //   function (Container $c) {
-    //     return new InvoiceService(
-    //       $c->get(SalesTaxService::class),
-    //       $c->get(PaymentGatewayService::class),
-    //       $c->get(EmailService::class)
-    //     );
-    //   }
-    // );
-    // static::$container->set(SalesTaxService::class, fn () => new SalesTaxService());
-    // static::$container->set(PaymentGatewayService::class, fn () => new PaymentGatewayService());
-    // static::$container->set(EmailService::class, fn () => new EmailService());
+    // $this->container->set(PaymentGatewayServiceInterface::class, fn (Container $c) => $c->get(PaymentGatewayService::class));
+
+    // $this->container->set(PaymentGatewayServiceInterface::class, PaymentGatewayService::class);
+    $this->container->set(PaymentGatewayServiceInterface::class, PaddlePayment::class);
   }
 
   public static function db(): DB
