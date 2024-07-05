bbfe267c6aed2bdfb042020dc7e7a27a2822cece
DaryllCulas <culasdaryll99@gmail.com>
2024-07-05 23:08:57 +0800

    Using Generator to avoid memory issues

--- advance_PHP/PHP_Generators/app/models/Ticket.php
+++ advance_PHP/PHP_Generators/app/models/Ticket.php
@@ -5,21 +5,21 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Model;
-
+use Generator;
 
 class Ticket extends Model
 {
-  public function all(): array
+  public function all(): Generator
   {
     $stmt = $this->db->query(
       'SELECT id, title, description, created_at, updated_at
        FROM Ticket'
     );
 
-    return $stmt->fetchAll();
+    // return $stmt->fetchAll();
 
-    // foreach ($stmt as $tickets) {
-    //   yield $tickets;
-    // }
+    foreach ($stmt as $tickets) {
+      yield $tickets;
+    }
   }
 }
