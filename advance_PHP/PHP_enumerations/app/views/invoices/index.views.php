0fa567f9f2b11d8561073847b45cafec700df33a
DaryllCulas <culasdaryll99@gmail.com>
2024-07-08 06:25:42 +0800

    Fetched invoiceStatus and add stylings with custmization

--- advance_PHP/PHP_enumerations/app/views/invoices/index.views.php
+++ advance_PHP/PHP_enumerations/app/views/invoices/index.views.php
@@ -6,33 +6,62 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Invoice</title>
 </head>
-<style>
+<table>
 
-</style>
+  <style>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    text-align: center;
+  }
 
-<body>
-  <h1>Invoices</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Invoice Number</th>
-        <th>Amount</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($invoices as $invoice) : ?>
-      <tr>
-        <td><?= $invoice->id ?></td>
-        <td><?= $invoice->invoiceNumber ?></td>
-        <td><?= $invoice->amount ?></td>
-        <td><?= $invoice->status ?></td>
-      </tr>
-      <?php endforeach ?>
-    </tbody>
+  table tr th,
+  table tr td {
+    border: 1px #eee solid;
+    padding: 5px;
+  }
 
-  </table>
-</body>
+  .color-green {
+    color: green;
+  }
+
+  .color-red {
+    color: red;
+  }
+
+  .color-gray {
+    color: gray;
+  }
+
+  .color-orange {
+    color: orange;
+  }
+  </style>
+
+  <body>
+    <h1>Invoices</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Invoice Number</th>
+          <th>Amount</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($invoices as $invoice) : ?>
+        <tr>
+          <td><?= $invoice->id ?></td>
+          <td><?= $invoice->invoiceNumber ?></td>
+          <td><?= $invoice->amount ?></td>
+          <td class="color-<?= \App\Enums\InvoiceStatus::tryFrom($invoice->status)->color() ?>">
+            <?= \App\Enums\InvoiceStatus::tryFrom($invoice->status)->toString() ?></td>
+        </tr>
+        <?php endforeach ?>
+      </tbody>
+
+    </table>
+  </body>
 
 </html>
