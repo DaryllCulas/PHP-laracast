71c8fcf45c9285c846fa1f3f7fb575dfa7c467b9
DaryllCulas <culasdaryll99@gmail.com>
2024-07-09 10:43:24 +0800

    Refactored => Enum for Put Method

--- advance_PHP/PHP_enumerations/app/controllers/Attributes/Put.php
+++ advance_PHP/PHP_enumerations/app/controllers/Attributes/Put.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace App\Controllers\Attributes;
 
 use App\Attributes\Route;
+use App\Enums\HttpMethod;
 
 
-#[\Attribute()]
+#[\Attribute(\Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 class Put extends \App\Controllers\Attributes\Route
 {
 
   public function __construct(string $routePath)
   {
-    parent::__construct($routePath, 'put');
+    parent::__construct($routePath, HttpMethod::Put);
   }
 }
