eadd1f01d30c5631cafa331fabafda2ddd2a057d
DaryllCulas <culasdaryll99@gmail.com>
2024-07-09 10:44:20 +0800

    Modified register->requestMethod to access its value

--- advance_PHP/PHP_enumerations/app/controllers/Attributes/Route.php
+++ advance_PHP/PHP_enumerations/app/controllers/Attributes/Route.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controllers\Attributes;
 
-
+use App\Enums\HttpMethod;
 use Attribute;
 
 
-#[Attribute]
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
 class Route implements \App\Controllers\Attributes\RouteInterface
 {
 
-  public function __construct(public string $routePath, public string $method = 'get')
+  public function __construct(public string $routePath, public HttpMethod $method = HttpMethod::Get)
   {
   }
 }
