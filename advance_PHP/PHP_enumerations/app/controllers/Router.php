e7bfb29fd1965692f78e0f657fd87fb43c08204b
DaryllCulas <culasdaryll99@gmail.com>
2024-07-09 10:45:50 +0800

    Modified register->requestMethod to access its value

--- advance_PHP/PHP_enumerations/app/controllers/Router.php
+++ advance_PHP/PHP_enumerations/app/controllers/Router.php
@@ -32,7 +32,7 @@ class Router
         foreach ($attributes as $attribute) {
           $route = $attribute->newInstance();
 
-          $this->register($route->method, $route->routePath, [$controller, $method->getName()]);
+          $this->register($route->method->value, $route->routePath, [$controller, $method->getName()]);
         }
       }
     }
