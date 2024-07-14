fdc1b28bad853bf63807bb1decb84df73c0b93d5
root <culasdaryll99@gmail.com>
2024-07-15 05:50:01 +0800

    Modified

--- advance_PHP/PHP_Doctrine_ORM_Entities_and_Data_Mapper/public/index.php
+++ advance_PHP/PHP_Doctrine_ORM_Entities_and_Data_Mapper/public/index.php
@@ -8,6 +8,9 @@ use App\Controllers\GeneratorExampleController;
 use App\Controllers\HomeController;
 use App\Controllers\InvoiceController;
 use App\Controllers\Router;
+use App\Entity\Invoice;
+use App\Entity\InvoiceItem;
+use App\Enums\InvoiceStatus;
 use App\Services\Container;
 
 
@@ -18,25 +21,48 @@ session_start();
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-define('STORAGE_PATH', __DIR__ . '/../storage');
-define('VIEW_PATH', __DIR__ . '/../app/views');
 
-$container = new Container();
-$routerObj = new Router($container);
+$items = [['Item 1', 1, 15], ['Item 2', 2, 7.5], ['Item 3', 4, 3.75]];
 
-$routerObj->registerRoutesFromControllerAttributes(
-  [
-    HomeController::class,
-    GeneratorExampleController::class,
-    InvoiceController::class
-
-  ]
-);
+$invoice = (new Invoice())
+  ->setAmount(45)
+  ->setInvoiceNumber('1')
+  ->setStatus(InvoiceStatus::PAID)
+  ->setCreatedAt(new DateTime());
 
 
-(new App(
-  $container,
-  $routerObj,
-  ['uri' => $_SERVER['REQUEST_URI'], 'method' => $_SERVER['REQUEST_METHOD']],
-  new Config($_ENV)
-))->run();
+
+foreach ($items as [$description, $quantity, $unitPrice]) {
+  $item = (new InvoiceItem())
+    ->setDescription($description)
+    ->setQuantity($quantity)
+    ->setUnitPrice($unitPrice);
+
+  $invoice->addItem($item);
+}
+
+
+
+
+// define('STORAGE_PATH', __DIR__ . '/../storage');
+// define('VIEW_PATH', __DIR__ . '/../app/views');
+
+// $container = new Container();
+// $routerObj = new Router($container);
+
+// $routerObj->registerRoutesFromControllerAttributes(
+//   [
+//     HomeController::class,
+//     GeneratorExampleController::class,
+//     InvoiceController::class
+
+//   ]
+// );
+
+
+// (new App(
+//   $container,
+//   $routerObj,
+//   ['uri' => $_SERVER['REQUEST_URI'], 'method' => $_SERVER['REQUEST_METHOD']],
+//   new Config($_ENV)
+// ))->run();
