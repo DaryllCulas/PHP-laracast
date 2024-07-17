42f4c1f23a917468d19071a34d1b7b4fd4d99e97
root <culasdaryll99@gmail.com>
2024-07-17 16:32:16 +0800

    OR-Mapping using foreach for fetching data from existing table

---
+++ advance_PHP/PHP_Active_Records_Pattern_using_Laravel_Eloquent/app/eloquent.example.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Enums\InvoiceStatus;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
+use Carbon\Carbon;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../eloquent.php';
+
+
+$invoice = new Invoice();
+
+$invoice->amount = 45;
+$invoice->invoice_number = 'INV001';
+$invoice->status = InvoiceStatus::PENDING;
+// $invoice->due_date = (new Carbon())->addDays(10);
+
+$invoice->save();
+
+$items = [['item 1', 1, 2], ['item 2', 3, 4.5], ['item 3', 5, 6.5]];
+
+
+// Iterate over the invoice items
+foreach ($items as [$description, $quantity, $unitPrice]) {
+
+  $item = new InvoiceItem();
+
+  $item->description = $description;
+  $item->quantity = $quantity;
+  $item->unit_price = $unitPrice;
+
+
+  $item->invoice()->associate($invoice);
+  $item->save();
+}
