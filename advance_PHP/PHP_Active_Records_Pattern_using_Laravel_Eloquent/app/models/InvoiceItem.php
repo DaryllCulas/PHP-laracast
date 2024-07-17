d5fd981425a4e1db7ebf267c5b1b60fba536c030
root <culasdaryll99@gmail.com>
2024-07-17 16:35:53 +0800

    DocBloc for InvoiceItem Properties

--- advance_PHP/PHP_Active_Records_Pattern_using_Laravel_Eloquent/app/models/InvoiceItem.php
+++ advance_PHP/PHP_Active_Records_Pattern_using_Laravel_Eloquent/app/models/InvoiceItem.php
@@ -7,9 +7,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $invoice_id
+ * @property string $description
+ * @property float $quantity
+ * @property float $unit_price
+ */
 
 class InvoiceItem extends Model
 {
+
   public $timestamps = false;
   protected $table = 'Invoice_items';
 
