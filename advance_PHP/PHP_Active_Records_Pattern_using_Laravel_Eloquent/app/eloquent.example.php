410b98ecd53e3a0e413d83bf903de906ff232aa1
root <culasdaryll99@gmail.com>
2024-07-17 17:02:34 +0800

    Use WHERE clause in queryBuilder

--- advance_PHP/PHP_Active_Records_Pattern_using_Laravel_Eloquent/app/eloquent.example.php
+++ advance_PHP/PHP_Active_Records_Pattern_using_Laravel_Eloquent/app/eloquent.example.php
@@ -11,31 +11,34 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 require_once __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../eloquent.php';
 
-Capsule::connection()->transaction(function () {
+// Capsule::connection()->transaction(function () {
 
-  $invoice = new Invoice();
+//   $invoice = new Invoice();
 
-  $invoice->amount = 45;
-  $invoice->invoice_number = 'INV001';
-  $invoice->status = InvoiceStatus::PENDING;
-  // $invoice->due_date = (new Carbon())->addDays(10);
+//   $invoice->amount = 45;
+//   $invoice->invoice_number = 'INV001';
+//   $invoice->status = InvoiceStatus::PENDING;
+//   // $invoice->due_date = (new Carbon())->addDays(10);
 
-  $invoice->save();
+//   $invoice->save();
 
-  $items = [['item 1', 1, 2], ['item 2', 3, 4.5], ['item 3', 5, 6.5]];
-
-
-  // Iterate over the invoice items
-  foreach ($items as [$description, $quantity, $unitPrice]) {
-
-    $item = new InvoiceItem();
-
-    $item->description = $description;
-    $item->quantity = $quantity;
-    $item->unit_price = $unitPrice;
+//   $items = [['item 1', 1, 2], ['item 2', 3, 4.5], ['item 3', 5, 6.5]];
 
 
-    $item->invoice()->associate($invoice);
-    $item->save();
-  }
-});
+//   // Iterate over the invoice items
+//   foreach ($items as [$description, $quantity, $unitPrice]) {
+
+//     $item = new InvoiceItem();
+
+//     $item->description = $description;
+//     $item->quantity = $quantity;
+//     $item->unit_price = $unitPrice;
+
+
+//     $item->invoice()->associate($invoice);
+//     $item->save();
+//   }
+// });
+
+$invoiceId = 1;
+Invoice::query()->where('id', $invoiceId)->update(['status' => InvoiceStatus::PAID]);
