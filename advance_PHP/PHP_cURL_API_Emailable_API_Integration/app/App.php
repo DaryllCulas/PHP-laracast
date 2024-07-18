ca3b14f9dac06fbba1163a58c19cddf98c31a517
root <culasdaryll99@gmail.com>
2024-07-18 10:42:26 +0800

    PHP cURL API- Emailable API Integration added

---
+++ advance_PHP/PHP_cURL_API_Emailable_API_Integration/app/App.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Config;
+use App\Controllers\Router;
+use App\Exceptions\RouteNotFoundException;
+use App\Views\View;
+use Dotenv\Dotenv;
+use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+
+class App
+{
+
+
+  public function __construct(protected Container $container, protected Router $routerObj, protected array $request, protected Config $config)
+  {
+  }
+
+  public function initDb(array $config)
+  {
+
+    $capsule = new Capsule;
+
+    $capsule->addConnection($config);
+    $capsule->setEventDispatcher(new Dispatcher($this->container));
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+  }
+
+
+
+  public function boot(): static
+  {
+    $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+
+    $this->config = new Config($_ENV);
+    $this->initDb($this->config->db);
+
+    return $this;
+  }
+
+
+  public function run()
+  {
+    try {
+      echo $this->routerObj->resolve($this->request['uri'], strtolower($this->request['method']));
+    } catch (RouteNotFoundException) {
+
+      http_response_code(404);
+
+      echo View::make('error/404');
+    }
+  }
+}
