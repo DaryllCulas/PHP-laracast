87c447a15281f6837d6ff33762612250ff2f960e
root <culasdaryll99@gmail.com>
2024-07-19 20:57:26 +0800

    Add EmailValidationService class with baseUrl and verify method

--- advance_PHP/PHP_refactor_cURL_to_Guzzle_with_Retry_logic(Multiple API integrations)/app/Services/Emailable/EmailValidationService.php
+++ advance_PHP/PHP_refactor_cURL_to_Guzzle_with_Retry_logic(Multiple API integrations)/app/Services/Emailable/EmailValidationService.php
@@ -6,20 +6,25 @@ namespace App\Services\Emailable;
 
 class EmailValidationService
 {
+  private string $baseUrl = 'https://api.emailable.com/v1/verify?';
+
+  public function __construct(private string $apiKey)
+  {
+  }
 
   public function verify(string $email): array
   {
     $handle = curl_init();
-    $apiKey = $_ENV['EMAILABLE_API_KEY'];
+
 
 
     $params = [
-      'api_key' => $apiKey,
+      'api_key' => $this->apiKey,
       'email' => $email
 
     ];
 
-    $url = 'https://api.emailable.com/v1/verify?' . http_build_query($params);
+    $url = $this->baseUrl . http_build_query($params);
 
     // set options
     curl_setopt($handle, CURLOPT_URL, $url);
