26d9b33ebe9baf1ab530a88d9114347ef3e31294
root <culasdaryll99@gmail.com>
2024-07-19 22:33:24 +0800

    Retry logic applied

--- advance_PHP/PHP_refactor_cURL_to_Guzzle_with_Retry_logic(Multiple API integrations)/app/Services/Emailable/EmailValidationService.php
+++ advance_PHP/PHP_refactor_cURL_to_Guzzle_with_Retry_logic(Multiple API integrations)/app/Services/Emailable/EmailValidationService.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace App\Services\Emailable;
 
+
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 
 class EmailValidationService
 {
@@ -16,13 +23,19 @@ class EmailValidationService
 
   public function verify(string $email): array
   {
+    $stack = HandlerStack::create();
+    $maxRetry = 3;
+
+    $stack->push($this->getRetryMiddleware($maxRetry));
+
+
     $client = new Client(
       [
         'base_uri' => $this->baseUrl,
-        'timeout' => 5
+        'timeout' => 5,
+        'handler' => $stack
       ]
     );
-
 
     $params = [
       'api_key' => $this->apiKey,
@@ -33,5 +46,36 @@ class EmailValidationService
     $response = $client->get('verify', ['query' => $params]);
 
     return json_decode($response->getBody()->getContents(), true);
+  }
+
+  private function getRetryMiddleware(int $maxRetry)
+  {
+    return Middleware::retry(
+      function (
+        int $retries,
+        RequestInterface $request,
+        ?ResponseInterface $response = null,
+        ?\RuntimeException $e = null
+      ) use ($maxRetry) {
+        if ($retries >= $maxRetry) {
+
+          return false;
+        }
+
+        if ($response && in_array($response->getStatusCode(), [500, 502, 503, 504, 404])) {
+          echo 'Retrying [' . $retries . ']' . '<br />' . 'status: ' . $response->getStatusCode() . PHP_EOL;
+
+          return true;
+        }
+
+        if ($e instanceof ConnectException) {
+          echo "Retrying [$retries], Connection Error" . PHP_EOL;
+
+          return true;
+        }
+
+        return false;
+      }
+    );
   }
 }
